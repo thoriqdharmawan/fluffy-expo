a3494b68df628de0bc739f54219c6db14cea7cd0
thoriqdharmawan <thoriqd21@gmail.com>
2023-04-25 00:03:33 +0700

    chore: add react-native-ui-lib

--- app/home/menu.js
+++ app/home/menu.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import { Text, View } from "react-native";
+import { Button, Dialog, PanningProvider } from "react-native-ui-lib";
 
 export default function Menu() {
+  const [visible, setVisible] = useState(false)
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Menu Page</Text>
+    <View>
+      <Text onPress={() => setVisible(true)}>Menu Page</Text>
+      <Button onPress={() => setVisible(true)} label="visible" size={Button.sizes.small} />
+
+      <Dialog
+        visible={visible}
+        onDismiss={() => setVisible(false)}
+        panDirection={PanningProvider.Directions.DOWN}
+      >
+        <Text text60>Content</Text>
+
+        <Button onPress={() => setVisible(false)} label="Click" size={Button.sizes.small} />
+      </Dialog>
     </View>
   );
 }
